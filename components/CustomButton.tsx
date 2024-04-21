7a2b55ebecd20ef7a6d716d0e101cb746826f874
valgeorg97 <valentingg1997@gmail.com>
2024-04-21 23:22:41 +0300

    Add NavBar component

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -5,11 +5,11 @@ import { CustomButtonProps } from '@/types';
 
 
 const CustomButton = ({ title, containerStyles, 
-    handleClick}:CustomButtonProps) => {
+    handleClick, btnType}:CustomButtonProps) => {
   return (
     <button
     disabled={false}
-    type={'button'}
+    type={btnType || 'button'}
     className={`custom-btn ${containerStyles}`}
     onClick={handleClick}
     >
