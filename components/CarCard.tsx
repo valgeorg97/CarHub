4fd7099ede470d0e4deecdc3c7d937c37c7694d8
valgeorg97 <valentingg1997@gmail.com>
2024-04-30 11:06:23 +0300

    Add CarDetails Modal

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -38,57 +38,43 @@ const CarCard = ({ car }: CarCardProps) => {
       </p>
       <div className='relatve w-full h-40 my-3 object-contain'>
         <Image src='/hero.png'
-          alt='Car' width={300} height={100} priority
+          alt='Car' width={260} height={50} priority
           className='object-contain' />
       </div>
       <div className='relative flex w-full mt-2'>
-        <div className='flex group-hover:invisible
-            w-full justify-between text-gray'>
-          <div className='flex flex-col justify-center
-                items-center gap-2'>
-            <Image src='/steering-wheel.svg' width={20}
-              height={20} alt='steering wheel' />
-            <p className='text-[14px]'>
-              {transmission === 'a' ? 'Automatic'
-                : 'Manual'}
-            </p>
-          </div>
-        </div>
-        <div className='flex group-hover:invisible
-            w-full justify-between text-gray'>
-          <div className='flex flex-col justify-center
-                items-center gap-2'>
-            <Image src='/tire.svg' width={20}
-              height={20} alt='tire' />
-            <p className='text-[14px]'>
-              {drive.toUpperCase()}
-            </p>
-          </div>
-        </div>
-        <div className='flex group-hover:invisible
-            w-full justify-between text-gray'>
-          <div className='flex flex-col justify-center
-                items-center gap-2'>
-            <Image src='/gas.svg' width={20}
-              height={20} alt='steering wheel' />
-            <p className='text-[14px]'>
-              {city_mpg} MPG
-            </p>
-          </div>
-        </div>
-        <div className='car-card__btn-container'>
-          <CustomButton 
-          title='View More'
-          containerStyles='w-full py-[16px]
-          rounded-full bg-primary-blue'
-          textStyles='text-white text-[14px] 
-          leading-[17px] font-bold'
-          rightIcon='/right-arrow.svg'
-          handleClick={() => setIsOpen(true)}
-          />
-        </div>
-
-      </div>
+  <div className='flex group-hover:invisible w-full justify-start text-gray'>
+    <div className='flex flex-col justify-center items-center gap-2'>
+      <Image src='/steering-wheel.svg' width={20} height={20} alt='steering wheel' />
+      <p className='text-[14px]'>
+        {transmission === 'a' ? 'Auto' : 'Manual'}
+      </p>
+    </div>
+  </div>
+  <div className='flex group-hover:invisible w-full justify-center text-gray'>
+    <div className='flex flex-col justify-center items-center gap-2'>
+      <Image src='/tire.svg' width={20} height={20} alt='tire' />
+      <p className='text-[14px]'>
+        {drive.toUpperCase()}
+      </p>
+    </div>
+  </div>
+  <div className='flex group-hover:invisible w-full justify-end text-gray'>
+    <div className='flex flex-col justify-center items-center gap-2'>
+      <Image src='/gas.svg' width={20} height={20} alt='steering wheel' />
+      <p className='text-[14px]'>
+        {city_mpg} MPG
+      </p>
+    </div>
+  </div>
+  <div className='car-card__btn-container'>
+    <CustomButton 
+      title='View More'
+      containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
+      textStyles='text-white text-[14px] leading-[17px] font-bold'
+      handleClick={() => setIsOpen(true)}
+    />
+  </div>
+</div>
       <CarDetails 
       isOpen={isOpen} 
       closeModal={ () => setIsOpen(false)}
